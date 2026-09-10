1844710f7e9047cb7f9aff79d446a95c55fa08dc
Guy D'Overbroeck's <guy.williamson@doverbroecks.com>
2026-09-10 22:18:12 +0100

    Create cylinder_volume.py
    A python script with comments for students to complete the code.

---
+++ cylinder_volume.py
@@ -0,0 +1,17 @@
+# Get the dimensions
+
+# Ask the user for the radius
+
+# Ask the user for the height
+
+
+# Calculate the volume
+
+# Calculate the area of the circular base
+
+# Multiply the base area by the height
+
+
+# Display the result
+
+# Show the volume to the user
